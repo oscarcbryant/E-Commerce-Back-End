0c0d9bdb7094fae89d852fedfdc31b038a20aebe
Oscar Bryant <oscarcbryant@gmail.com>
2022-03-12 22:55:20 +1100

    testing tags route in insomnia

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Tag.update(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
