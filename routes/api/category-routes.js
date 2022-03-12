098d38c887d474b4e2ad0da2381a9d39fdcb6a3b
Oscar Bryant <oscarcbryant@gmail.com>
2022-03-12 16:31:13 +1100

    resolving issues with routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,7 +43,9 @@ router.post('/', async (req, res) => {
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData); 
+    console.log(hello);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
